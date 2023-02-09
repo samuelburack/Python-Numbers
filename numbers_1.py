3c298bd56fc52eab99c2462ce74f75fee7623747
samuelburack <21sburack@gmail.com>
2023-02-08 18:49:11 -0700

    random numbers

--- numbers_1.py
+++ numbers_1.py
@@ -1,1 +1,10 @@
 #Python Numbers
+a_number = 10 #Created datatype int
+a_float_number = 1.2
+a_number = float(a_number)
+
+# print(a_number + a_float_number)
+
+import random
+
+print(random.randrange(0,900))
